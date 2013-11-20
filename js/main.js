fdbc345fd0626e04d46d7aefcf1d355fe977e477
chinpira <jason.mullinax@gmail.com>
2013-11-20 18:45:16 -0500

    Completed Part 6

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+    showAlert: function(message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     },
 
     initialize: function() {
-        this.store = new LocalStorageStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        var self = this;
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        this.store = new LocalStorageStore(function() {
+            $('body').html(new HomeView(self.store).render().el);    
+        });
     }
 
 };
